c93db5a3a0c910ffadfc78b2d90e9e7f7f5e864a
Mike Chang <mchang95@gmail.com>
2021-08-26 17:41:16 -0700

    adds docstrings for all routes in app.py

--- app.py
+++ app.py
@@ -30,26 +30,23 @@ connect_db(app)
 ##############################################################################
 # Auth Routes / Functions
 
-@app.route('/login', methods=['POST'])
-def login():
-    # breakpoint()
-    username = request.json['username']
-    password = request.json['password']
-    
-    user = User.authenticate(username, password)
-    if user:
-        token = createJWT(user)
-        return (jsonify(user=user.serialize(),token=token))
-    return jsonify(error='Invalid login'), 401
-
 def createJWT(user):
-    """ Creates JWT token with username and admin in payload. """
+    """ Given user instance, creates and returns JWT token with username and
+        admin in payload.
+    """
+
     payload = { 'username':user.username, 'is_admin':user.is_admin }
     token = jwt.encode(payload, app.config.get('SECRET_KEY'), algorithm='HS256')
     return token
 
 def authenticateJWT():
-    """ Verifies that JWT is valid. """
+    """Verifies that JWT is valid. Returns user information if valid or empty
+        dictionary if invalid.
+
+        If valid, returns:
+            user: {id, username, first_name, last_name, email, phone}
+    """
+
     auth_headers = request.headers.get('Authorization', '').split()
     if len(auth_headers) != 2:
         return None
@@ -65,6 +62,64 @@ def authenticateJWT():
         return None
     return None
 
+@app.route('/users', methods=["POST"])
+def sign_up(): 
+    """Handles user sign up. If valid form data and no duplicate, returns
+        serialized user information and token. Otherwise, error.
+    
+        Accepts: 
+            { username, password, first_name, last_name, email, phone }
+
+        Returns: 
+            {
+                user: {username, first_name, last_name, email, phone},
+                token: "token"
+            }
+    """
+
+    username = request.json["username"]
+    password = request.json["password"]
+    first_name = request.json["first_name"]
+    last_name = request.json["last_name"]
+    email = request.json["email"]
+    phone = request.json["phone"]
+
+    duplicate_check = User.query.filter(User.username.like(username)).one_or_none()
+    if duplicate_check:
+        return jsonify(error='Username taken'), 400
+
+    try: 
+        user = User.signup(
+                username=username,
+                password=password,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                phone=phone,
+            )
+        token = createJWT(user)
+        return jsonify(user=user.serialize(), token=token)
+
+    except IntegrityError as error:
+        return (jsonify(error=error))
+
+@app.route('/login', methods=['POST'])
+def login():
+    """If valid credentials presented in JSON, returns token, otherwise 401 error.
+
+        Accepts: {username, password}
+    """
+
+    username = request.json['username']
+    password = request.json['password']
+    
+    user = User.authenticate(username, password)
+    if user:
+        token = createJWT(user)
+        return (jsonify(user=user.serialize(),token=token))
+    return jsonify(error='Invalid login'), 401
+
+
 ##############################################################################
 # Listing Routes
 
@@ -72,6 +127,8 @@ def authenticateJWT():
 def get_listings():
     """If search term included, gets filtered listings. Otherwise,
         gets all listings.
+
+        Returns: listings: [{id, title, price, details, address, host_id, photos}, ...]
     """
 
     search = request.args.get('term')
@@ -87,9 +144,13 @@ def get_listings():
 
 @app.route('/listings', methods=["POST"])
 def add_listing():
-    """Handle add listing.
+    """Creates new listing and adds to DB. Uploads photos to AWS S3 and adds to
+        listing photos table. Returns serialized listing in JSON.
 
-    Create new listing and add to DB.
+        Returns: 
+            listing: {id, title, price, details, address, host_id, photos}
+            where photos is: 
+                [photo_url, photo_url, photo_url, ...]
     """
     
     user = authenticateJWT()
@@ -99,13 +160,6 @@ def add_listing():
             file = request.files.get(key)
             output = upload_file_to_s3(file, S3_BUCKET)
             img_urls.append(output)
-        # print(img_urls)
-        # breakpoint()
-        # file = request.files["photo"]
-
-        # output = upload_file_to_s3(file, S3_BUCKET)
-        # photos = str(output)
-        # print("PHOTOS AFTER UPLOAD", photos)
 
         title = request.form.getlist("title")[0]
         price = request.form.getlist("price")[0]
@@ -140,7 +194,11 @@ def add_listing():
 
 @app.route('/listings/<int:id>')
 def get_listing(id): 
-    """Gets listing by id."""
+    """Gets listing by id. Returns serialized listing details in JSON.
+
+        Returns: 
+            listing: {id, title, price, details, address, host_id, photos}
+    """
 
     listing = Listing.query.get_or_404(id)
 
@@ -152,58 +210,16 @@ def get_listing(id):
 ##############################################################################
 # User Routes
 
-@app.route('/users', methods=["POST"])
-def sign_up(): 
-    """Handles user sign up."""
-
-    username = request.json["username"]
-    password = request.json["password"]
-    first_name = request.json["first_name"]
-    last_name = request.json["last_name"]
-    email = request.json["email"]
-    phone = request.json["phone"]
-
-    duplicate_check = User.query.filter(User.username.like(username)).one_or_none()
-    if duplicate_check:
-        return jsonify(error='Username taken'), 400
-
-    try: 
-        user = User.signup(
-                username=username,
-                password=password,
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-                phone=phone,
-            )
-        token = createJWT(user)
-        return jsonify(user=user.serialize(), token=token)
-
-    except IntegrityError as error:
-        return (jsonify(error=error))
-
-
-# @app.route('/login', methods=["POST"])
-# def login(): 
-#     """Authenticates user and, if valid credentials, returns token."""
-
-#     username = request.json["username"]
-#     password = request.json["password"]
-
-#     user = User.authenticate(username, password)
-
-#     if user: 
-#         token = createJWT(user)
-#         return jsonify(user=user.serialize(), token=token)
-#     else: 
-#         return ("Invalid login", 400)
-
-
-
 @app.route('/users')
 def get_users():
     """If search term included, gets filtered users. Otherwise,
-        gets all users.
+        gets all users. Returns list of serialized users in JSON.
+
+        Accepts: 
+            q: "term" (optional)
+        
+        Returns: 
+            users: [{username, first_name, last_name, email, phone}, ...]
     """
 
     search = request.args.get('q')
@@ -221,7 +237,12 @@ def get_users():
 
 @app.route('/users/<int:id>')
 def get_user(id): 
-    """Gets user by id."""
+    """Gets user by id. If found, returns serialized user information in JSON.
+        Otherwise, 404.
+    
+        Returns: 
+            user: {username, first_name, last_name, email, phone}
+    """
 
     user = User.query.get_or_404(id)
 
@@ -234,7 +255,14 @@ def get_user(id):
 
 @app.route('/messages/<int:id>', methods=['POST'])
 def send_message(id):
-    """Send message to user with id."""
+    """Send message from current user to user of id in url params. If valid
+        token, returns serialized message details in JSON.
+
+        Returns: 
+            msg: {id, text, timestamp, to_user_id, to_user, from_user_id, from_user}
+            Where to_user and from_user is: 
+                {username, first_name, last_name, email, phone}
+    """
 
     text = request.json['message']
     curr_user = authenticateJWT()
@@ -255,8 +283,15 @@ def send_message(id):
 
 @app.route('/messages')
 def get_messages():
-    """Get all of current user's messages."""
-    # breakpoint()
+    """Get all of current user's messages. If valid token, returns serialized
+        list of all messages.
+
+        Returns: 
+            msgs: [{id, text, timestamp, to_user_id, to_user, from_user_id, from_user}, ...]
+            Where to_user and from_user is: 
+                {username, first_name, last_name, email, phone}
+    """
+
     curr_user = authenticateJWT()
     if curr_user:
         msgs = Message.query.filter((Message.from_user_id == curr_user.id) | (Message.to_user_id == curr_user.id)).all()
@@ -268,7 +303,14 @@ def get_messages():
 
 @app.route('/messages/<int:id>')
 def get_conversion_with_user(id):
-    """Get current user's messages to or from user with id."""
+    """Get current user's messages to or from user of id in url params. If
+        valid token, returns serialized list of messages.
+
+        Returns: 
+            msgs: [{id, text, timestamp, to_user_id, to_user, from_user_id, from_user}, ...]
+            Where to_user and from_user is: 
+                {username, first_name, last_name, email, phone}
+    """
 
     curr_user = authenticateJWT()
     if curr_user:
